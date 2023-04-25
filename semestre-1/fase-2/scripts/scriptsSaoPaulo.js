158a98097c36d243cc01709a31e26cf715b24598
Alexandre Shyjada <alexandre.shyjada@ufba.br>
2023-04-24 23:54:40 -0300

    Add: link para sites de compra de passagens

--- semestre-1/fase-2/scripts/scriptsSaoPaulo.js
+++ semestre-1/fase-2/scripts/scriptsSaoPaulo.js
@@ -3,56 +3,56 @@ const holtelListArray = [
     id: 1,
     name: "IBIS Styles São Paulo Anhembi",
     availability: "Disponível",
-    url: "./sao-paulo.html",
+    url: "https://www.booking.com/hotel/br/ibis-styles-sao-paulo-anhembi.pt-br.html",
     imageURL: "./assets/hoteis/ibis.png",
   },
   {
     id: 2,
     name: "Confort Ibirapuera",
-    availability: "Indisponível",
-    url: "#",
+    availability: "Disponível",
+    url: "https://www.booking.com/hotel/br/comfort-ibirapuera.pt-br.html",
     imageURL: "./assets/hoteis/confort.png",
   },
   {
     id: 3,
     name: "Blue Tree Premium Morumbi",
-    availability: "Indisponível",
-    url: "#",
+    availability: "Disponível",
+    url: "https://www.booking.com/hotel/br/blue-tree-premium-morumbi.pt-br.html",
     imageURL: "./assets/hoteis/blue_tree.png",
   },
   {
     id: 4,
     name: "Mercure São Paulo Pamplona",
-    availability: "Indisponível",
-    url: "#",
+    availability: "Disponível",
+    url: "https://www.booking.com/hotel/br/apartments-sao-paulo-pamplona.pt-br.html",
     imageURL: "./assets/hoteis/pamplona.png",
   },
   {
     id: 5,
     name: "Sheraton São Paulo WTC Hotel",
-    availability: "Indisponível",
-    url: "#",
+    availability: "Disponível",
+    url: "https://www.booking.com/hotel/br/sao-paulo-wtc.pt-br.html",
     imageURL: "./assets/hoteis/sheraton.png",
   },
   {
     id: 6,
     name: "Bourbon Convention Ibirapuera",
-    availability: "Indisponível",
-    url: "#",
+    availability: "Disponível",
+    url: "https://www.booking.com/hotel/br/bourbon-convention-ibirapuera.pt-br.html",
     imageURL: "./assets/hoteis/bourbon.png",
   },
   {
     id: 7,
     name: "Golden Tulip Paulista Plaza",
-    availability: "Indisponível",
-    url: "#",
+    availability: "Disponível",
+    url: "https://www.tripadvisor.com.br/LocationPhotoDirectLink-g303631-d301508-i280141509-Radisson_Paulista-Sao_Paulo_State_of_Sao_Paulo.html",
     imageURL: "./assets/hoteis/golden_tulip.png",
   },
   {
     id: 8,
     name: "Renaissance São Paulo Hotel",
-    availability: "Indisponível",
-    url: "#",
+    availability: "Disponível",
+    url: "https://www.marriott.com.br/hotels/travel/saobr-renaissance-sao-paulo-hotel/",
     imageURL: "./assets/hoteis/renaissance.png",
   },
 ];
@@ -65,7 +65,7 @@ function renderHoltel(hotelList) {
   const citysHtml = hotelList
     .map(
       (hotel) => `
-        <a href="${hotel.url}" class="card ${
+        <a href="${hotel.url}" target="_blank" class="card ${
           hotel.availability === "Disponível" ? "disponivel" : "indisponivel"
       }" style="background-image: url(${hotel.imageURL});">
             <div class="localeHeader">
